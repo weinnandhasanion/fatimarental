e7cede09c547594fb9646351ca16a5e16ed3905b
weinnand <weinnandondre.adlaon.hasanion@enrise-corp.co.jp>
2022-05-06 11:05:21 +0800

    add reservations table

--- admin/pages/reservations.php
+++ admin/pages/reservations.php
@@ -1,5 +1,13 @@
 <?php
 include_once './redirect.php';
+include './../../services/connect.php';
+
+$sql = "SELECT * FROM reservations";
+$res = $conn->query($sql);
+$reservations = [];
+if ($res) {
+  $reserviations = $res->fetch_all(MYSQLI_ASSOC);
+}
 ?>
 
 <!doctype html>
@@ -7,131 +15,62 @@ include_once './redirect.php';
 
 <?php include './../templates/head.php'?>
 
-
 <body>
-
-
-
-
   <div class="wrapper">
-
-
     <div class="body-overlay"></div>
 
     <?php include './../templates/nav.php' ?>
 
-
-
-
     <!-- Page Content  -->
     <div id="content">
 
-    <?php include './../templates/topnav.php'?>
-
-
+      <?php include './../templates/topnav.php'?>
 
       <div class="main-content">
         <div class="row ">
-          <div class="col-lg-7 col-md-12">
+          <div class="col-sm-12">
             <div class="card" style="min-height: 485px">
-              <div class="card-header card-header-text">
-                <h4 class="card-title">Tenant Status</h4>
-                <p class="category">New Tentat Rent on 21th December, 2020</p>
+              <div class="card-header card-header-text d-flex justify-content-start align-items-center">
+                <h4 class="card-title flex-grow-1">Reservations</h4>
               </div>
               <div class="card-content table-responsive">
-                <table class="table table-hover">
+                <table class="table table-hover" id='rooms-table'>
                   <thead class="text-primary">
                     <tr>
-                      <th>ID</th>
                       <th>Name</th>
-                      <th>Payment Status</th>
-                      <th>Room</th>
+                      <th>Email Address</th>
+                      <th>Move Date</th>
+                      <th>Message</th>
                       <th>Action</th>
                     </tr>
                   </thead>
                   <tbody>
+                    <?php foreach ($reservations as $res) {?>
                     <tr>
-                      <td>1</td>
-                      <td>Michael James Antiporta</td>
-                      <td>UnPaid</td>
-                      <td>Room 1</td>
-                      <td style="cursor: pointer;"><i class="material-icons">edit</i>
-                        <i class="material-icons">delete</i>
+                      <td class="align-middle"><?=$res['name']?></td>
+                      <td class="align-middle">
+                        <?=$res['email_address']?>
+                      </td>
+                      <td class="align-middle"><?=date("F d, Y", strtotime($res['move_date']))?></td>
+                      <td class="align-middle"><?=$res['message']?></td>
+                      <td class="align-middle">
+                        <button class="btn btn-link btn-small" data-id="<?=$room['id']?>"
+                          onclick="viewRoomDetails($(this).attr('data-id'))">Details</button>
+                        <button class="btn btn-small btn-link" data-id="<?=$room['id']?>"
+                          onclick="updateRoom($(this).attr('data-id'))">Update</button>
+                        <button class="btn btn-small btn-link text-success"
+                          onclick="window.location.href = `./billing.php?room_id=<?=$room['id']?>`">Bill</button>
                       </td>
                     </tr>
-                    <tr>
-                      <td>1</td>
-                      <td>Michael James Antiporta</td>
-                      <td>UnPaid</td>
-                      <td>Room 2</td>
-                      <td style="cursor: pointer;"><i class="material-icons">edit</i>
-                        <i class="material-icons">delete</i>
-                      </td>
-                    </tr>
-                    <tr>
-                      <td>1</td>
-                      <td>Michael James Antiporta</td>
-                      <td>UnPaid</td>
-                      <td>Room 3</td>
-                      <td style="cursor: pointer;"><i class="material-icons">edit</i>
-                        <i class="material-icons">delete</i>
-                      </td>
-                    </tr>
-                    <tr>
-                      <td>1</td>
-                      <td>Michael James Antiporta</td>
-                      <td>UnPaid</td>
-                      <td>Room 4</td>
-                      <td style="cursor: pointer;"><i class="material-icons">edit</i>
-                        <i class="material-icons">delete</i>
-                      </td>
-                    </tr>
-                    <tr>
-                      <td>1</td>
-                      <td>Michael James Antiporta</td>
-                      <td>UnPaid</td>
-                      <td>Room 5</td>
-                      <td style="cursor: pointer;"><i class="material-icons">edit</i>
-                        <i class="material-icons">delete</i>
-                      </td>
-                    </tr>
-                    <tr>
-                      <td>1</td>
-                      <td>Michael James Antiporta</td>
-                      <td>UnPaid</td>
-                      <td>Room 6</td>
-                      <td style="cursor: pointer;"><i class="material-icons">edit</i>
-                        <i class="material-icons">delete</i>
-                      </td>
-                    </tr>
-                    <tr>
-                      <td>1</td>
-                      <td>Michael James Antiporta</td>
-                      <td>UnPaid</td>
-                      <td>Room 7</td>
-                      <td style="cursor: pointer;"><i class="material-icons">edit</i>
-                        <i class="material-icons">delete</i>
-                      </td>
-                    </tr>
+                    <?php }?>
                   </tbody>
                 </table>
               </div>
             </div>
           </div>
-
-
-
         </div>
-
-
-
       </div>
     </div>
-
-
-
-
-
 
     <?php include './../templates/scripts.php' ?>
 
@@ -158,11 +97,6 @@ include_once './redirect.php';
 
     });
     </script>
-
-
-
-
-
 </body>
 
 </html>
