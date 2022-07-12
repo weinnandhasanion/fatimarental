b6f280785afaf89f4cd38308c5af2d6fc14608b0
weinnandhasanion <weinnandhasanion@gmail.com>
2022-07-12 21:00:05 +0800

    fix reports

--- admin/pages/reports.php
+++ admin/pages/reports.php
@@ -6,22 +6,29 @@ $month = date("n");
 $previousMonth = date("n", strtotime(" -1 month"));
 $sql = "SELECT SUM(amount) AS monthTotal FROM payments WHERE MONTH(date_added) = $month";
 $res = $conn->query($sql);
-$monthTotal = $res->fetch_assoc()['monthTotal'];
+$monthTotal = $res->fetch_assoc()['monthTotal'] ?? 0;
 $sql = "SELECT SUM(amount) AS monthTotal FROM payments WHERE MONTH(date_added) = $previousMonth";
 $res = $conn->query($sql);
 $previousMonthTotal = $res->fetch_assoc()['monthTotal'] ?? 0;
-$monthDifferential = ceil((1 - $previousMonthTotal / $monthTotal) * 100);
+$monthDifferential = 0;
+
+if ($monthTotal > 0 || $previousMonthTotal > 0) {
+    $monthDifferential = ceil((1 - $previousMonthTotal / $monthTotal) * 100);
+}
 
 $year = date("Y");
 $previousYear = date("Y", strtotime(" -1 year"));
-echo $previousYear;
 $sql = "SELECT SUM(amount) AS yearTotal FROM payments WHERE YEAR(date_added) = $year";
 $res = $conn->query($sql);
-$yearTotal = $res->fetch_assoc()['yearTotal'];
+$yearTotal = $res->fetch_assoc()['yearTotal'] ?? 0;
 $sql = "SELECT SUM(amount) AS yearTotal FROM payments WHERE YEAR(date_added) = $previousYear";
 $res = $conn->query($sql);
 $previousYearTotal = $res->fetch_assoc()['yearTotal'] ?? 0;
-$yearDifferential = ceil((1 - $previousYearTotal / $yearTotal) * 100);
+$yearDifferential = 0;
+
+if ($yearTotal > 0 || $previousYearTotal > 0) {
+    $yearDifferential = ceil((1 - $previousYearTotal / $yearTotal) * 100);
+}
 
 $getPercentageClass = function ($kind) use ($monthDifferential, $yearDifferential) {
     if ($kind === 'year') {
@@ -67,15 +74,15 @@ $getPercentageClass = function ($kind) use ($monthDifferential, $yearDifferentia
                 <h1 class=" font-weight-light">Php <?=$monthTotal?>.00</h1>
                 <p class="text-secondary">Total sales for the month of July</p>
                 <p class="mt-3 lead"><strong class="<?=$getPercentageClass('month')?>"><?=$monthDifferential?>% differential</strong> from last month</p>
-                <p class="text-secondary"><?= date("F", strtotime(" -1 month")) ?> sales: <strong>P<?=$previousMonthTotal?>.00</strong></p>
+                <p class="text-secondary"><?=date("F", strtotime(" -1 month"))?> sales: <strong>P<?=$previousMonthTotal?>.00</strong></p>
               </div>
             </div>
             <div class="bg-light col-sm-6">
               <div class="card-body text-center p-5">
                 <h1 class="font-weight-light">Php <?=$yearTotal?>.00</h1>
                 <p class="text-secondary">Total sales for the current year</p>
-                <p class="mt-3 lead"><strong class="<?=$getPercentageClass('year')?>"><?= $yearDifferential ?>% differential</strong> from last year</p>
-                <p class="text-secondary"><?= date("Y", strtotime(" -1 year")) ?> sales: <strong>P<?=$previousYearTotal?>.00</strong></p>
+                <p class="mt-3 lead"><strong class="<?=$getPercentageClass('year')?>"><?=$yearDifferential?>% differential</strong> from last year</p>
+                <p class="text-secondary"><?=date("Y", strtotime(" -1 year"))?> sales: <strong>P<?=$previousYearTotal?>.00</strong></p>
               </div>
             </div>
 
