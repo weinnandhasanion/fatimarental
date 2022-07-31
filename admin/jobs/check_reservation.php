13200692ffb673f00d3c4dcdeee9fba67edd79d1
weinnandhasanion <weinnandhasanion@gmail.com>
2022-07-31 19:13:45 +0800

    add fix for reservation job

--- admin/jobs/check_reservation.php
+++ admin/jobs/check_reservation.php
@@ -2,7 +2,7 @@
 include "./../../services/connect.php";
 
 $sql = "SELECT id, reservation_account_expiry_date FROM tenants 
-        WHERE `status`= 1";
+        WHERE `status`= 1 AND account_status = 0";
 $res = $conn->query($sql);
 $tenants = $res->fetch_all(MYSQLI_ASSOC);
 
@@ -10,7 +10,7 @@ $now = strtotime(date(DATE_RSS));
 
 foreach ($tenants as $tenant) {
   if ($now > strtotime($tenant['reservation_account_expiry_date'])) {
-    $sql = "UPDATE tenants SET account_status = 2 WHERE id = '".$tenant['id']."'";
+    $sql = "UPDATE tenants SET account_status = 2, `status` = 2 WHERE id = '".$tenant['id']."'";
     $conn->query($sql);
   }
 }
