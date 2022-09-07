b441d63473def413def7abfa04692a08908de9ac
weinnandhasanion <weinnandhasanion@gmail.com>
2022-09-07 22:04:06 +0800

    add readme and some comments

--- index.php
+++ index.php
@@ -4,6 +4,10 @@ if (!isset($_SESSION['user'])) {
     header("Location: ./tenant/index.php");
 }
 
+// Check if user in session has user_type property.
+// If user_type property exists, it means that the user
+// in the session is a admin. If not, the user in
+// the session is an tenant.
 if (array_key_exists('user_type', $_SESSION['user'])) {
     switch ($_SESSION['user']['user_type']) {
         default:
