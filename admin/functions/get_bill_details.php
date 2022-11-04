d6b022b436c3c604534999241bf2def61f8263f8
weinnandhasanion <weinnandhasanion@gmail.com>
2022-11-05 00:44:35 +0800

    rm bill-to

--- admin/functions/get_bill_details.php
+++ admin/functions/get_bill_details.php
@@ -1,14 +1,24 @@
 <?php 
 include "./../../services/connect.php";
 
-$reference_id = $_GET['reference_id'];
 $status = 200;
+$res;
 
-$sql = "SELECT b.*, r.room_name FROM bills AS b
-  INNER JOIN rooms AS r
-  ON b.room_id = r.id
-  WHERE b.reference_id = '$reference_id'";
-$res = $conn->query($sql);
+if (isset($_GET['bill_id'])) {
+  $bill_id = $_GET['bill_id'];
+  $sql = "SELECT b.*, r.room_name FROM bills AS b
+    INNER JOIN rooms AS r
+    ON b.room_id = r.id
+    WHERE b.id = $bill_id";
+  $res = $conn->query($sql);
+} else {
+  $reference_id = $_GET['reference_id'];
+  $sql = "SELECT b.*, r.room_name FROM bills AS b
+    INNER JOIN rooms AS r
+    ON b.room_id = r.id
+    WHERE b.reference_id = '$reference_id'";
+  $res = $conn->query($sql);
+}
 
 if ($res->num_rows > 0) {
   $data = $res->fetch_assoc();
--- admin/functions/get_payment_details.php
+++ admin/functions/get_payment_details.php
@@ -4,7 +4,7 @@ include "./../../services/connect.php";
 $id = $_GET['id'];
 
 $sql = "SELECT p.*, b.reference_id AS bill_reference_id,
-  r.room_name, t.first_name, t.last_name
+  r.room_name
   FROM payments AS p
   INNER JOIN bill_payments AS bp
   ON p.id = bp.payment_id
@@ -12,8 +12,6 @@ $sql = "SELECT p.*, b.reference_id AS bill_reference_id,
   ON bp.bill_id = b.id
   INNER JOIN rooms AS r
   ON r.id = b.room_id
-  INNER JOIN tenants AS t
-  ON t.id = b.bill_to_tenant_id
   WHERE p.id = " . intval($id);
 $res = $conn->query($sql);
 if ($res) {
