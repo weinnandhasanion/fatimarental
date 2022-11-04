1454ccf0c9a946ca649bce39d44b2a3460506558
weinnandhasanion <weinnandhasanion@gmail.com>
2022-11-04 23:59:23 +0800

    change billing functionality

--- admin/functions/add_bill.php
+++ admin/functions/add_bill.php
@@ -2,96 +2,183 @@
 include "./../../services/connect.php";
 session_start();
 
-$room_id = $_POST['room_id'];
-$room_charge = $_POST['room_charge'];
-$water_bill = $_POST['water_bill'];
-$electricity_bill = $_POST['electricity_bill'];
-$start_period = $_POST['start_period'];
-$end_period = $_POST['end_period'];
-$bill_to_tenant_id = $_POST['bill_to'];
-$addtl_charges = [];
-
-if (isset($_POST['additional_charges']) && count($_POST['additional_charges']) > 0) {
-    $addtl_charges = $_POST['additional_charges'];
-}
-
-if (empty($_POST['water_bill'])) $water_bill = "0";
-if (empty($_POST['electricity_bill'])) $electricity_bill = "0";
-
+$data = $_POST['data'] ? json_decode(json_encode(json_decode($_POST['data'])), true) : [];
 $errors = [];
 $status = 422;
 
-if (!preg_match("/^[0-9]*$/", $water_bill)) {
-    $errors['water_bill'] = 'Water bill should not contain any letters.';
+foreach ($data as $roomKey => $room) {
+    // Copy data to errors
+    $errors[$roomKey] = $room;
+    foreach ($errors[$roomKey] as $subKey => $_) {
+        $errors[$roomKey][$subKey] = "";
+    }
+    
+    // Unset unnecessary values for errors
+    unset($errors[$roomKey]["room_price"]);
+    unset($errors[$roomKey]["charges"]);
 }
 
+foreach ($data as $roomKey => $room) {
+    // Generate errors
+    foreach ($data[$roomKey] as $key => $value) {
+        if ($key === "charges") continue;
+        if (empty($value)) {
+            $errors[$roomKey][$key] = "Please provide a value";
+        } else if (!str_contains($key, "name") && !is_numeric($value)) {
+            $errors[$roomKey][$key] = "Value should only contain digits.";
+        }
 
-if (!isset($bill_to_tenant_id)) {
-    $errors['bill_to'] = 'Please select a tenant to bill.';
-}
-
-if (!preg_match("/^[0-9]*$/", $electricity_bill)) {
-    $errors['electricity_bill'] = 'Electricity bill should not contain any letters.';
-}
-
-
-if (strtotime($start_period) > strtotime($end_period)) {
-    $errors['start_period'] = 'Start period must be a date before end period.';
-} else {
-    $start_period = date("Y-m-d", strtotime($start_period));
-}
-
-if (strtotime($end_period) < strtotime($start_period)) {
-    $errors['end_period'] = 'End period must be a date after start period.';
-} else {
-    $end_period = date("Y-m-d", strtotime($end_period));
-}
-
-foreach ($addtl_charges as $idx => $addtl) {
-    if ($addtl["name-$idx"] === '' && !empty($addtl["charge-$idx"])) {
-        $errors["name-$idx"] = 'Please enter an item.';
+        if (empty(($errors[$roomKey][$key]))) {
+            unset($errors[$roomKey][$key]);
+        }
     }
 
-    if ($addtl["charge-$idx"] === '' && !empty($addtl["name-$idx"])) {
-        $errors["name-$idx"] = 'Please enter an amount.';
-    }
-
-    if (!preg_match("/^[0-9]*$/", $addtl["charge-$idx"])) {
-        $errors["charge-$idx"] = 'Additional charge should not contain any letters.';
+    // Unset room if no errors
+    if (count($errors[$roomKey]) < 1) {
+        unset($errors[$roomKey]);
     }
 }
 
 if (count($errors) < 1) {
-    $total = intval($room_charge) + intval($electricity_bill) + intval($water_bill);
-    $sql = "INSERT INTO bills (reference_id, room_id, admin_id, bill_to_tenant_id, room_charge, electricity_bill, water_bill, start_period, end_period)
-    VALUES ('" . md5(uniqid('', true)) . "', '$room_id', " . $_SESSION['user']['id'] . ", '$bill_to_tenant_id', '$room_charge', '$electricity_bill', '$water_bill', '$start_period', '$end_period')";
-    $res = $conn->query($sql);
+    $sqls = [];
+    $status = 200;
 
-    if ($res) {
-        $bill_id = $conn->insert_id;
-
-        foreach ($addtl_charges as $idx => $charge) {
-            $total += intval($charge["charge-$idx"]);
-            $sql = "INSERT INTO additional_charges (bill_id, `name`, charge) VALUES ($bill_id, '".$charge["name-$idx"]."', '".$charge["charge-$idx"]."')";
-            $res = $conn->query($sql);
+    foreach ($data as $roomId => $room) {
+        $electricity_bill = intval($room['electricity_bill']);
+        $water_bill = intval($room['water_bill']);
+        $start_period = date("Y-m-01");
+        $end_period = date("Y-m-t");
+        $room_charge = intval($room['room_price']);
+        $total = intval($room_charge) + intval($electricity_bill) + intval($water_bill);
+        foreach (['charges', 'room_price', 'water_bill', 'electricity_bill'] as $key) {
+            unset($data[$roomId][$key]);
         }
-
-        $sql = "UPDATE bills SET `total_amount` = $total WHERE id = $bill_id";
-        $conn->query($sql);
-
-        $sql = "SELECT * FROM tenants WHERE room_id = $room_id";
+        
+        $sql = "INSERT INTO bills (reference_id, room_id, admin_id, room_charge, electricity_bill, water_bill, start_period, end_period)
+                VALUES ('" . md5(uniqid('', true)) . "', '$roomId', " . $_SESSION['user']['id'] . ", '$room_charge', '$electricity_bill', '$water_bill', '$start_period', '$end_period')";
         $res = $conn->query($sql);
-        $rows = $res->fetch_all(MYSQLI_ASSOC);
 
-        foreach ($rows as $row) {
-            $sql = "INSERT INTO tenant_bills (tenant_id, bill_id) VALUES ('".$row['id']."', ".intval($bill_id).")";
-            $res = $conn->query($sql);
+        if ($res) {
+            $bill_id = $conn->insert_id;
+
+            if (count($data[$roomId]) > 0) {
+                $vals = array_values($data[$roomId]);
+                $charges = [];
+                $i = 0;
+                
+                while ($i < count($vals)) {
+                    $charges[] = [$vals[$i], $vals[++$i]];
+                    $i++;
+                }
+    
+                foreach ($charges as $ch) {
+                    $total += intval($ch[1]);
+                    $sql = "INSERT INTO additional_charges (bill_id, `name`, charge) VALUES ($bill_id, '".$ch[0]."', '".$ch[1]."')";
+                    $conn->query($sql);
+                }
+            }
+
+            $sql = "UPDATE bills SET `total_amount` = $total WHERE id = $bill_id";
+            $conn->query($sql);
         }
-        $status = 200;
     }
 }
 
 echo json_encode([
-    'errors' => $errors,
-    'status' => $status,
+    "status" => $status,
+    "errors" => $errors
 ]);
+
+// $room_id = $_POST['room_id'];
+// $room_charge = $_POST['room_charge'];
+// $water_bill = $_POST['water_bill'];
+// $electricity_bill = $_POST['electricity_bill'];
+// $start_period = $_POST['start_period'];
+// $end_period = $_POST['end_period'];
+// $bill_to_tenant_id = $_POST['bill_to'];
+// $addtl_charges = [];
+
+// if (isset($_POST['additional_charges']) && count($_POST['additional_charges']) > 0) {
+//     $addtl_charges = $_POST['additional_charges'];
+// }
+
+// if (empty($_POST['water_bill'])) $water_bill = "0";
+// if (empty($_POST['electricity_bill'])) $electricity_bill = "0";
+
+// $errors = [];
+// $status = 422;
+
+// if (!preg_match("/^[0-9]*$/", $water_bill)) {
+//     $errors['water_bill'] = 'Water bill should not contain any letters.';
+// }
+
+
+// if (!isset($bill_to_tenant_id)) {
+//     $errors['bill_to'] = 'Please select a tenant to bill.';
+// }
+
+// if (!preg_match("/^[0-9]*$/", $electricity_bill)) {
+//     $errors['electricity_bill'] = 'Electricity bill should not contain any letters.';
+// }
+
+
+// if (strtotime($start_period) > strtotime($end_period)) {
+//     $errors['start_period'] = 'Start period must be a date before end period.';
+// } else {
+//     $start_period = date("Y-m-d", strtotime($start_period));
+// }
+
+// if (strtotime($end_period) < strtotime($start_period)) {
+//     $errors['end_period'] = 'End period must be a date after start period.';
+// } else {
+//     $end_period = date("Y-m-d", strtotime($end_period));
+// }
+
+// foreach ($addtl_charges as $idx => $addtl) {
+//     if ($addtl["name-$idx"] === '' && !empty($addtl["charge-$idx"])) {
+//         $errors["name-$idx"] = 'Please enter an item.';
+//     }
+
+//     if ($addtl["charge-$idx"] === '' && !empty($addtl["name-$idx"])) {
+//         $errors["name-$idx"] = 'Please enter an amount.';
+//     }
+
+//     if (!preg_match("/^[0-9]*$/", $addtl["charge-$idx"])) {
+//         $errors["charge-$idx"] = 'Additional charge should not contain any letters.';
+//     }
+// }
+
+// if (count($errors) < 1) {
+//     $total = intval($room_charge) + intval($electricity_bill) + intval($water_bill);
+//     $sql = "INSERT INTO bills (reference_id, room_id, admin_id, bill_to_tenant_id, room_charge, electricity_bill, water_bill, start_period, end_period)
+//     VALUES ('" . md5(uniqid('', true)) . "', '$room_id', " . $_SESSION['user']['id'] . ", '$bill_to_tenant_id', '$room_charge', '$electricity_bill', '$water_bill', '$start_period', '$end_period')";
+//     $res = $conn->query($sql);
+
+//     if ($res) {
+//         $bill_id = $conn->insert_id;
+
+//         foreach ($addtl_charges as $idx => $charge) {
+//             $total += intval($charge["charge-$idx"]);
+//             $sql = "INSERT INTO additional_charges (bill_id, `name`, charge) VALUES ($bill_id, '".$charge["name_$idx"]."', '".$charge["charge_$idx"]."')";
+//             $res = $conn->query($sql);
+//         }
+
+//         $sql = "UPDATE bills SET `total_amount` = $total WHERE id = $bill_id";
+//         $conn->query($sql);
+
+//         $sql = "SELECT * FROM tenants WHERE room_id = $room_id";
+//         $res = $conn->query($sql);
+//         $rows = $res->fetch_all(MYSQLI_ASSOC);
+
+//         foreach ($rows as $row) {
+//             $sql = "INSERT INTO tenant_bills (tenant_id, bill_id) VALUES ('".$row['id']."', ".intval($bill_id).")";
+//             $res = $conn->query($sql);
+//         }
+//         $status = 200;
+//     }
+// }
+
+// echo json_encode([
+//     'errors' => $errors,
+//     'status' => $status,
+// ]);
--- admin/templates/nav.php
+++ admin/templates/nav.php
@@ -3,52 +3,53 @@ $page = basename($_SERVER['PHP_SELF']);
 ?>
 
 <nav id="sidebar">
-  <div class="sidebar-header">
-    <h3><span>Fatima Admin</span></h3>
-  </div>
-  <ul class="list-unstyled components">
-    <li class="<?= $page === 'dashboard.php' ? "active" : "" ?>">
-      <a href="./dashboard.php" class="dashboard"><i class="material-icons">dashboard</i><span>Dashboard</span></a>
-    </li>
-    
-    <div class="small-screen navbar-display">
-      <li class="d-lg-none d-md-block d-xl-none d-sm-block">
-        <a href="#"><i class="material-icons">person</i><span>user</span></a>
-      </li>
+    <div class="sidebar-header">
+        <h3><span>Fatima Admin</span></h3>
     </div>
+    <ul class="list-unstyled components">
+        <li class="<?= $page === 'dashboard.php' ? "active" : "" ?>">
+            <a href="./dashboard.php" class="dashboard"><i
+                    class="material-icons">dashboard</i><span>Dashboard</span></a>
+        </li>
 
-    <li class="<?= $page === 'reservations.php' ? "active" : "" ?>">
-      <a href="./reservations.php"> <i class="material-icons">library_books</i><span>Reservations</span></a>
-    </li>
+        <div class="small-screen navbar-display">
+            <li class="d-lg-none d-md-block d-xl-none d-sm-block">
+                <a href="#"><i class="material-icons">person</i><span>user</span></a>
+            </li>
+        </div>
 
-    <li class="<?= $page === 'tenants.php' ? "active" : "" ?>">
-      <a href="./tenants.php">
-        <i class="material-icons">group</i><span>Tenants</span></a>
-    </li>
+        <li class="<?= $page === 'reservations.php' ? "active" : "" ?>">
+            <a href="./reservations.php"> <i class="material-icons">library_books</i><span>Reservations</span></a>
+        </li>
 
-    <li class="<?= $page === 'records.php' ? "active" : "" ?>">
-      <a href="./records.php">
-        <i class="material-icons">feed</i><span>Records</span></a>
-    </li>
+        <li class="<?= $page === 'tenants.php' ? "active" : "" ?>">
+            <a href="./tenants.php">
+                <i class="material-icons">group</i><span>Tenants</span></a>
+        </li>
 
-    <li class="<?= $page === 'billing.php' ? "active" : "" ?>">
-      <a href="./billing.php">
-        <i class="material-icons">content_copy</i><span>Billing</span></a>
-    </li>
+        <li class="<?= $page === 'records.php' ? "active" : "" ?>">
+            <a href="./records.php">
+                <i class="material-icons">feed</i><span>Records</span></a>
+        </li>
 
-    <li class="<?= in_array($page, ['rooms.php', 'edit_room_images.php']) ? "active" : "" ?>">
-      <a href="./rooms.php">
-        <i class="material-icons">meeting_room</i><span>Rooms</span></a>
-    </li>
+        <li class="<?= in_array($page, ['billing.php', 'bill_rooms.php']) ? "active" : "" ?>">
+            <a href="./billing.php">
+                <i class="material-icons">content_copy</i><span>Billing</span></a>
+        </li>
 
-    <li class="<?= $page === 'payments.php' ? "active" : "" ?>">
-      <a href="./payments.php">
-        <i class="material-icons">payments</i><span>Payments</span></a>
-    </li>
+        <li class="<?= in_array($page, ['rooms.php', 'edit_room_images.php']) ? "active" : "" ?>">
+            <a href="./rooms.php">
+                <i class="material-icons">meeting_room</i><span>Rooms</span></a>
+        </li>
 
-    <li class="<?= $page === 'reports.php' ? "active" : "" ?>">
-      <a href="./reports.php">
-        <i class="material-icons">receipt_long</i><span>Income Reports</span></a>
-    </li>
-  </ul>
+        <li class="<?= $page === 'payments.php' ? "active" : "" ?>">
+            <a href="./payments.php">
+                <i class="material-icons">payments</i><span>Payments</span></a>
+        </li>
+
+        <li class="<?= $page === 'reports.php' ? "active" : "" ?>">
+            <a href="./reports.php">
+                <i class="material-icons">receipt_long</i><span>Income Reports</span></a>
+        </li>
+    </ul>
 </nav>
