5366ed0806d709634e55566e64c80f35f5f524f9
jay <ajdellera@gmail.com>
2022-11-07 18:36:50 +0800

    front end changes

--- tenant/index.php
+++ tenant/index.php
@@ -47,7 +47,9 @@ $rooms = array_map(function($room) use ($conn) {
             </div>
             <div class="address">
                 <i class="fas fa-map-marker-alt"></i>
-                <span>321-North Escario Street Kamputhaw Cebu City</span>
+                <span>321-North Escario Street Kamputhaw Cebu City</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <i class="fas fa-phone"></i>
+                <span>417-1686 / (0918) 948 1290</span>
             </div>
         </div>
     </section>
@@ -125,7 +127,7 @@ $rooms = array_map(function($room) use ($conn) {
                 <center><h3>A Home Where You Can Find Peace </center>
                     </h3>&nbsp; &nbsp;
                     <h2></h2>
-                    <p style="color:black;"align="justify"> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; One of the current problems that Cebu City faces is the interruption of water supply from time to
+                    <p style="color:black;"align="justify" align="left"> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; One of the current problems that Cebu City faces is the interruption of water supply from time to
                         time.
                         In Barangay Kamputhaw, MCWD turns on the water supply between 12 am to 5 am daily
                         but sometimes for less time than that. This rotating water interruption
@@ -140,7 +142,7 @@ $rooms = array_map(function($room) use ($conn) {
                         Quality and Mineral Content Tests to ensure that boarders can have access to clean water 24/7.
                 </div>
                 <div class="right">
-                    <img src="./assets/h6.jpg" alt="" style="width: 60%;">
+                    <img  align="middle" src="./assets/h6.jpg" alt="" style="width: 60%;">
                 </div>
             </div>
         </div>
@@ -151,7 +153,7 @@ $rooms = array_map(function($room) use ($conn) {
         <div class="container">
             <div class="heading">
                 <h5>OUR ROOMS</h5>
-                <h2>Fascinating rooms</h2>
+                <h2>Practical & Budget-Friendly</h2>
             </div>
             <div class="content flex mtop">
                 <div class="left grid2">
@@ -184,7 +186,7 @@ $rooms = array_map(function($room) use ($conn) {
                     </div>
                 </div>
                 <div class="right">
-                    <img src="./assets/IMG_7317.JPG" alt="">
+                    <img src="./assets/h3.JPG" alt="">
                 </div>
             </div>
         </div>
@@ -390,18 +392,11 @@ $rooms = array_map(function($room) use ($conn) {
 
             <div class="content grid  top">
                 <div class="box">
-                    <div class="logo">
-                        <img src="./assets/logo.png" alt="">
-                    </div>
-                    <div class="social flex">
-                        <i class="fab fa-facebook-f"></i>
-                        <i class="fab fa-twitter"></i>
-                        <i class="fab fa-instagram"></i>
-                    </div>
+        
                 </div>
 
                 <div class="box">
-                    <h2>Quick Links</h2>
+                    <h2 align="left">Quick Links</h2>
                     <ul>
                         <li><i class="fas fa-angle-double-right"></i>Reservation</li>
                         <li><i class="fas fa-angle-double-right"></i>FAQ</li>
@@ -418,14 +413,14 @@ $rooms = array_map(function($room) use ($conn) {
                 </div>
 
                 <div class="box">
-                    <h2>Services</h2>
+                    <h2>Contact Us:</h2>
                     <div class="icon flex">
                         <div class="i">
                             <i class="fas fa-map-marker-alt"></i>
                         </div>
                         <div class="text">
                             <h3>Address</h3>
-                            <p>321-N Escario Street Kamputhaw Cebu City</p>
+                            <p>321-North Escario Street Kamputhaw Cebu City</p>
                         </div>
                     </div>
                     <div class="icon flex">
@@ -434,16 +429,10 @@ $rooms = array_map(function($room) use ($conn) {
                         </div>
                         <div class="text">
                             <h3>Phone</h3>
-                            <p>4171 6862</p>
+                            <p>417-1686 / (0918) 948 1290</p>
                         </div>
                     </div>
-                    <div class="icon flex">
-                        <div class="i">
-                            <i class="far fa-envelope"></i>
-                        </div>
-                        <div class="text">
-                            <h3>Email</h3>
-                            <p>fatimarental@gmail.com</p>
+                    
                         </div>
                     </div>
                 </div>
