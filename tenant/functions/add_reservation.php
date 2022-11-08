725c465fa9d92bd69ed6a9bb3cbb26f9b8c96f39
weinnandhasanion <weinnandhasanion@gmail.com>
2022-11-09 07:39:48 +0800

    add validation for reservations

--- tenant/functions/add_reservation.php
+++ tenant/functions/add_reservation.php
@@ -3,31 +3,53 @@ include './../../services/connect.php';
 include_once './../../admin/functions/uuid.php';
 
 $status = 422;
-$room_id = intval($_POST['room_id']);
-$first_name = $_POST['first_name'];
-$middle_initial = $_POST['middle_initial'];
-$last_name = $_POST['last_name'];
-$gender = $_POST['gender'];
-$email_address = $_POST['email_address'];
-$contact_number = $_POST['contact_number'];
-$move_date = $_POST['move_date'];
-$message = $_POST['message'];
+$error = null;
+$now = strtotime(date("Y-m-d"));
 
-if (empty($message)) $message = null;
-
-$id = guidv4();
-$status = 1;
-$sql = "INSERT INTO tenants (id, room_id, first_name, last_name, middle_initial, gender, `status`, email_address, contact_number, reservation_account_expiry_date)
-  VALUES ('$id', $room_id, '$first_name', '$last_name', '$middle_initial', ".intval($gender).", $status, '$email_address', '$contact_number', DATE_ADD(NOW(), INTERVAL 3 DAY))";
-$res = $conn->query($sql);
-
-if ($res) {
-  $sql = "INSERT INTO reservations (room_id, tenant_id, move_date, `message`)  
-  VALUES ($room_id, '$id', '$move_date', '$message')";
-
-  $res = $conn->query($sql);
-  if ($res) $status = 200;
+if (!isset($_POST['room_id'])) {
+  $error = "Please choose a room!";
+} else if (!isset($_POST['first_name'])) {
+  $error = "Please enter your first name.";
+} else if (!isset($_POST['last_name'])) {
+  $error = "Please enter your last name.";
+} else if (!isset($_POST['gender'])) {
+  $error = "Please enter your gender.";
+} else if (!isset($_POST['email_address'])) {
+  $error = "Please enter a valid email address.";
+} else if (!isset($_POST['contact_number']) && !preg_match("/^(09|\+639)\d{9}$/", $_POST['contact_number'])) {
+  $error = "Please enter a valid phone number.";
+} else if (isset($_POST['move_date']) && $now >= strtotime(date($_POST['move_date']))) {
+  $error = "Please choose a move date greater than the current date.";
 }
 
-echo json_encode(['status' => $status]);
+if ($error === null) {
+  $room_id = intval($_POST['room_id']);
+  $first_name = $_POST['first_name'];
+  $middle_initial = $_POST['middle_initial'] ?? "";
+  $last_name = $_POST['last_name'];
+  $gender = $_POST['gender'];
+  $email_address = $_POST['email_address'];
+  $contact_number = $_POST['contact_number'];
+  $move_date = $_POST['move_date'];
+  $message = $_POST['message'];
+  
+  if (empty($message)) $message = null;
+  
+  $id = guidv4();
+  $status = 1;
+  $sql = "INSERT INTO tenants (id, room_id, first_name, last_name, middle_initial, gender, `status`, email_address, contact_number, reservation_account_expiry_date)
+    VALUES ('$id', $room_id, '$first_name', '$last_name', '$middle_initial', ".intval($gender).", $status, '$email_address', '$contact_number', DATE_ADD(NOW(), INTERVAL 3 DAY))";
+  $res = $conn->query($sql);
+  
+  if ($res) {
+    $sql = "INSERT INTO reservations (room_id, tenant_id, move_date, `message`)  
+    VALUES ($room_id, '$id', '$move_date', '$message')";
+  
+    $res = $conn->query($sql);
+    if ($res) $status = 200;
+  }
+  
+}
+
+echo json_encode(['status' => $status, 'error' => $error]);
 ?>
